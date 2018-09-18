3b0d04ab08d56ea31e929084679e95f5c3fe0f9d
Sean Ali <sean@alicographics.com>
2018-09-18 15:32:32 -0500

    Changed model naming, and modified the .gitignore to exclude .pyc files.

--- brewbombers/comps/admin.py
+++ brewbombers/comps/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Judge_group
+from .models import JudgeGroup
 from .models import Judge
 
 
 # Register your models here.
-admin.site.register(Judge_group)
+admin.site.register(JudgeGroup)
 admin.site.register(Judge)
