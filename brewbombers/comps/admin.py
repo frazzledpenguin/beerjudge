decf7dc0c6895ff77a7cebf353d90a530989d60b
Sean Ali <sean@alicographics.com>
2018-09-18 15:27:28 -0500

    Added models for judges and judge groups.

--- brewbombers/comps/admin.py
+++ brewbombers/comps/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Judge_group
+from .models import Judge
+
 
 # Register your models here.
+admin.site.register(Judge_group)
+admin.site.register(Judge)
--- brewbombers/comps/models.py
+++ brewbombers/comps/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class Judge_group(models.Model):
+    name    = models.CharField(max_length=250)
+
+    def __str__(self):
+        return '%s' % (self.name)
+
+    class Meta:
+        verbose_name_plural = "Judge groups"
+
+
+class Judge(models.Model):
+    # This table will hold the info for each judge
+    first_name      = models.CharField(max_length=250)
+    last_name       = models.CharField(max_length=250)
+    judge_group     = models.ForeignKey(Judge_group, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s' % (self.name)
+
+    class Meta:
+        verbose_name_plural = "Judge group"
