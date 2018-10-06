d0de2628b74bff540b390f0684891a133223151d
Sean Ali <sean@alicographics.com>
2018-10-06 11:57:53 -0500

    Updated models. Set proper foriegn key and added username and password to the Judge model.

--- brewbombers/comps/models.py
+++ brewbombers/comps/models.py
@@ -2,24 +2,26 @@ from django.db import models
 
 
 # Create your models here.
-class JudgeGroup(models.Model):
-    name    = models.CharField(max_length=250)
-
-    def __str__(self):
-        return '%s' % (self.name)
-
-    class Meta:
-        verbose_name_plural = "Judge groups"
-
-
 class Judge(models.Model):
     # This table will hold the info for each judge
     first_name      = models.CharField(max_length=250)
     last_name       = models.CharField(max_length=250)
-    judge_group     = models.ForeignKey(JudgeGroup, on_delete=models.CASCADE)
+    user_name       = models.CharField(max_length=250)
+    password        = models.CharField(max_length=250)
 
     def __str__(self):
         return '%s' % (self.name)
 
     class Meta:
         verbose_name_plural = "Judges"
+
+
+class JudgeGroup(models.Model):
+    name    = models.CharField(max_length=250)
+    user    = models.ForeignKey(Judge, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s' % (self.name)
+
+    class Meta:
+        verbose_name_plural = "Judge groups"
